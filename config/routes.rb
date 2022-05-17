b660c4ad7ab731a3f39019464df8b7bca7a298bd
gaelle jopwou <jgaelle@yahoo.fr>
2022-05-17 15:55:51 +0200

    done without css or bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "task/:id", to: "tasks#show", as: :task
+  get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  get "task/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "task/:id", to: "tasks#update"
+  delete "task/:id", to: "tasks#destroy"
 end
